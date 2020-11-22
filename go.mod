aeb39825667fbdb1b425d5089f3de3a39931ebea
silence <imsilence@outlook.com>
2020-11-22 18:59:38 +0800

    feature: two_sum solution
    Signed-off-by: silence <imsilence@outlook.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/imsilence/leetcode
+
+go 1.15
---
+++ two_sum_test.go
@@ -0,0 +1,53 @@
+package leetcode
+
+import (
+	"sort"
+	"testing"
+)
+
+type TwoSumCase struct {
+	name   string
+	nums   []int
+	target int
+	result []int
+}
+
+func (c *TwoSumCase) ok(r []int) bool {
+
+	if len(c.result) != len(r) {
+		return false
+	}
+
+	sort.Ints(c.result)
+	sort.Ints(r)
+
+	for i, v := range c.result {
+		if v != r[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func createTwoSumTestCase(t *testing.T, c *TwoSumCase) {
+	t.Helper()
+	result := twoSum(c.nums, c.target)
+	if !c.ok(result) {
+		t.Error(c.name, c, result)
+	}
+}
+
+func TestTwoSum(t *testing.T) {
+	cases := []*TwoSumCase{
+		{
+			"case 1",
+			[]int{2, 7, 11, 15},
+			9,
+			[]int{0, 1},
+		},
+	}
+
+	for _, c := range cases {
+		createTwoSumTestCase(t, c)
+	}
+}
