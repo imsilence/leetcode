289723fc5e8eb6b95f4967371fff49553747dcf7
silence <imsilence@outlook.com>
2020-11-22 19:00:36 +0800

    feature: add_two_numbers solution
    Signed-off-by: silence <imsilence@outlook.com>

---
+++ add_two_numbers_test.go
@@ -0,0 +1,47 @@
+package leetcode
+
+import "testing"
+
+type AddTwoNumbersCase struct {
+	name   string
+	left   *ListNode
+	right  *ListNode
+	result *ListNode
+}
+
+func (c *AddTwoNumbersCase) ok(r *ListNode) bool {
+	result := c.result
+	for result != nil && r != nil {
+		if result.Val != r.Val {
+			return false
+		}
+		result = result.Next
+		r = r.Next
+	}
+
+	return result == nil && r == nil
+}
+
+func createAddTwoNumberTestCase(t *testing.T, c *AddTwoNumbersCase) {
+	t.Helper()
+	result := addTwoNumbers(c.left, c.right)
+	if !c.ok(result) {
+		t.Error(c.name, c, result)
+	}
+}
+
+func TestAddTwoNumbers(t *testing.T) {
+	cases := []*AddTwoNumbersCase{
+		{
+			"case 1",
+			&ListNode{2, &ListNode{4, &ListNode{3, nil}}},
+			&ListNode{5, &ListNode{6, &ListNode{4, nil}}},
+			&ListNode{7, &ListNode{0, &ListNode{8, nil}}},
+		},
+	}
+
+	for _, c := range cases {
+		createAddTwoNumberTestCase(t, c)
+	}
+
+}
---
+++ node.go
@@ -0,0 +1,7 @@
+package leetcode
+
+// ListNode Node
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
