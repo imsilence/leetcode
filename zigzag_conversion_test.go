9a9757c80ba5b7d00ec09f68168fcd1560099191
silence <imsilence@outlook.com>
2020-11-22 19:23:02 +0800

    feature: zigzag_conversion solution
    Signed-off-by: silence <imsilence@outlook.com>

---
+++ zigzag_conversion_test.go
@@ -0,0 +1,51 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+type ZigzagConversionCase struct {
+	name   string
+	text   string
+	rows   int
+	result string
+}
+
+func (c *ZigzagConversionCase) ok(r string) bool {
+	return c.result == r
+}
+
+func createZigzagConversionTestCase(t *testing.T, c *ZigzagConversionCase) {
+	t.Helper()
+	result := convert(c.text, c.rows)
+	if !c.ok(result) {
+		t.Error(c.name, c, result)
+	}
+}
+
+func TestConvert(t *testing.T) {
+	cases := []*ZigzagConversionCase{
+		// {
+		// 	"case 1",
+		// 	"LEETCODEISHIRING",
+		// 	3,
+		// 	"LCIRETOESIIGEDHN",
+		// },
+		// {
+		// 	"case 2",
+		// 	"LEETCODEISHIRING",
+		// 	4,
+		// 	"LDREOEIIECIHNTSG",
+		// },
+		{
+			"case 3",
+			"AB",
+			1,
+			"AB",
+		},
+	}
+
+	for _, c := range cases {
+		createZigzagConversionTestCase(t, c)
+	}
+}
